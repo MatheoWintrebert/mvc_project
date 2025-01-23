cc643bfecdc855cc12c35254ed1f91883963d5e9
MatheoWintrebert <matheo.wintrebert@orange.fr>
2025-01-23 10:09:41 +0100

    just repared smthing

--- src/views/header.php
+++ src/views/header.php
@@ -9,11 +9,10 @@
   <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet"
     integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
 </head>
-<?php if ($title !== "Login" && $title !== "Register") { ?>
 
-  <body>
-    <?php
-      /** @var string */
-      echo $header;
-    ?>
-    <main>
+<body>
+  <?php
+  /** @var string */
+  echo $header;
+  ?>
+  <main>
