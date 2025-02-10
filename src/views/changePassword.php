29019e48ae610edfb9b93bcd73e59718f569577a
Zerdov <juleswillig08@gmail.com>
2025-02-10 11:55:39 +0100

    Modification des vues

--- src/views/changePassword.php
+++ src/views/changePassword.php
@@ -5,24 +5,15 @@
     <?php echo isset($alert) && !empty($alert) ? '<div class="alert alert-danger" role="alert">'.$alert.'</div>' : ""; ?>
     <div class="mb-3">
       <label for="current-password" class="form-label">Mot de passe actuel:</label>
-      <div class="input-group">
-        <input type="password" id="current-password" name="current-password" class="form-control"/>
-        <button type="button" class="btn btn-outline-secondary">Click me</button>
-      </div>
+      <input type="password" id="current-password" name="current-password" class="form-control"/>
     </div>
     <div class="mb-3">
       <label for="current-password" class="form-label">Nouveau mot de passe:</label>
-      <div class="input-group">
-        <input type="password" id="new-password" name="new-password" class="form-control"/>
-        <button type="button" class="btn btn-outline-secondary">Click me</button>
-      </div>
+      <input type="password" id="new-password" name="new-password" class="form-control"/>
     </div>
     <div class="mb-3">
       <label for="current-password" class="form-label">Confirmation nouveau de passe:</label>
-      <div class="input-group">
-        <input type="password" id="conf-new-password" name="conf-new-password" class="form-control"/>
-        <button type="button" class="btn btn-outline-secondary">Click me</button>
-      </div>
+      <input type="password" id="conf-new-password" name="conf-new-password" class="form-control"/>
     </div>
     <button type="submit" class="btn btn-primary">Valider</button>
   </form>
--- src/views/register.php
+++ src/views/register.php
@@ -1,24 +1,22 @@
 <?php declare(strict_types = 1); ?>
 
 <div class="container">
-    <?php echo $errorAlert !== "" ? '<div class="alert alert-danger" role="alert">' . $errorAlert . '</div>' : ""; ?>
     <form id="registerForm" name="registerForm" method="post" action="./?action=register">
         <h1>Créer un compte</h1>
+        <?php echo $errorAlert !== "" ? '<div class="alert alert-danger" role="alert">' . $errorAlert . '</div>' : ""; ?>
         <div class="mb-3">
             <label for="email" class="form-label">Adresse mail</label>
-            <input type="email" class="form-control" id="email" name="email" placeholder="exemple@email.com">
+            <input type="email" class="form-control" id="email" name="email" placeholder="exemple@email.com" />
         </div>
         <div class=" mb-3">
             <label for="password" class="form-label">Mot de passe</label>
-            <input type="password" class="form-control" id="password" name="password" placeholder="••••••••">
+            <input type="password" class="form-control" id="password" name="password" placeholder="••••••••" />
         </div>
         <div class=" mb-3">
             <label for="verifyPassword" class="form-label">Vérifier le mot de passe</label>
-            <input type="password" class="form-control" id="verifyPassword" name="verifyPassword"
-                placeholder="••••••••">
+            <input type="password" class="form-control" id="verifyPassword" name="verifyPassword" placeholder="••••••••" />
         </div>
         <button type="submit" class="btn btn-primary mb-3">Envoyer</button>
     </form>
-    <p>Déjà un compte ? <a class="link-primary link-offset-2 link-underline-opacity-25 link-underline-opacity-100-hover"
-            href="?action=login">Se connecter</a></p>
+    <p>Déjà un compte ? <a href="?action=login">Se connecter</a></p>
 </div>
